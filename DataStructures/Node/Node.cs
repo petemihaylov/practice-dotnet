076669d822f16a158b7b341f1285cf4b2b8d0475
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-05-22 12:43:58 +0200

    Node

---
+++ DataStructures/Node/Node.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Node
+{
+    class Node<Type>
+    {
+        private Type value;
+        private Node<Type> next; // pointer to the next value
+
+        public Node(Type value)
+        {
+            this.value = value;
+        }
+
+        public Type Value
+        {
+            get { return this.value; }
+            set { this.value = value; }
+        }
+        public Node<Type> Next
+        {
+            get { return this.next; }
+            set { this.next = value; }
+        }
+
+
+
+
+    }
+}
---
+++ DataStructures/Node/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Node
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node<int> firstNode = new Node<int>(5);
+            Node<int> secondNode = new Node<int>(10);
+
+            firstNode.Next = secondNode;
+
+            Node<int> currentNode = firstNode;
+
+            while(currentNode != null)
+            {
+                Console.WriteLine(currentNode.Value);
+                currentNode = currentNode.Next;
+            }
+
+        }
+    }
+}
