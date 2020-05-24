7ecca211ddac3be7cd6163301842599566882a7e
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-05-24 18:14:27 +0200

    post-order pre-order in-order

--- DataStructures/Trees/BinaryTree.cs
+++ DataStructures/Trees/BinaryTree.cs
@@ -2,22 +2,64 @@
 
 public class BinaryTree<T>
 {
+    public T Value { get; private set; }
+    public BinaryTree<T> Left { get; set; }
+    public BinaryTree<T> Right { get; set; }
+
     public BinaryTree(T value, BinaryTree<T> leftChild = null, BinaryTree<T> rightChild = null)
     {
+        this.Value = value;
+        this.Left = leftChild;
+        this.Right = rightChild;
     }
 
     public void PrintIndentedPreOrder(int indent = 0)
     {
-        throw new NotImplementedException();
+
+        this.PrintIndentedPreOrder(this, indent);
+    }
+
+    private void PrintIndentedPreOrder(BinaryTree<T> node, int indent)
+    {
+        if(node == null)
+        {
+            return;
+        }
+        Console.WriteLine($"{new string(' ',indent)}{node.Value}");
+        this.PrintIndentedPreOrder(node.Left, indent + 2);
+        this.PrintIndentedPreOrder(node.Right, indent + 2);
+
     }
 
     public void EachInOrder(Action<T> action)
     {
-        throw new NotImplementedException();
+        this.EachInOrder(this, action);
+    }
+    private void EachInOrder(BinaryTree<T> node, Action<T> action)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        this.EachInOrder(node.Left, action);
+        action(node.Value);
+        this.EachInOrder(node.Right, action);
     }
 
     public void EachPostOrder(Action<T> action)
     {
-        throw new NotImplementedException();
+        this.EachPostOrder(this, action);
     }
+    private void EachPostOrder(BinaryTree<T> node, Action<T> action)
+    {
+        if (node == null)
+        {
+            return;
+        }
+        this.EachPostOrder(node.Left, action);
+        this.EachPostOrder(node.Right, action);
+        action(node.Value);
+    }
+
 }
