965126e5d58ddbd6c7c69f80f2dd8c6ebd9af916
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-05-25 22:16:39 +0200

    binary search tree skele

---
+++ DataStructures/BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinarySearchTree
+{
+    public class BinarySearchTree<T> where T : IComparable<T>
+    {
+
+        public BinarySearchTree()
+        {
+
+        }
+
+        public void Insert(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteMin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BinarySearchTree<T> Search(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Range(T startRange, T endRange)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EachInOrder(Action<T> action)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
