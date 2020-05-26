ea0d4d2b3b6e2c56bb9586894a2b7cab872831e8
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-05-26 08:24:43 +0200

    serching algo

--- DataStructures/BinarySearchTree/BinarySearchTree.cs
+++ DataStructures/BinarySearchTree/BinarySearchTree.cs
@@ -5,18 +5,75 @@ namespace BinarySearchTree
 {
     public class BinarySearchTree<T> where T : IComparable<T>
     {
-
+        public Node root;
         public BinarySearchTree()
         {
-
+            this.root = null;
         }
 
         public void Insert(T value)
         {
-            throw new NotImplementedException();
+            if(this.root == null)
+            {
+                root = new Node(value);
+                return;
+            }
+
+            Node parent = null;
+            Node current = this.root;
+
+            while(current != null)
+            {
+                int compare = current.Value.CompareTo(value);
+
+                if(compare > 0)
+                {
+                    parent = current;
+                    current = current.Left;
+                }else if(compare < 0)
+                {
+                    parent = current;
+                    current = current.Right;
+                }else
+                {
+                    return;
+                }
+            }
+            Node newNode = new Node(value);
+            if (parent.Value.CompareTo(value) > 0)
+            {
+                parent.Left = newNode;
+            }
+            else
+            {
+                parent.Right = newNode;
+            }
+
+
         }
 
-        public bool Contains(T value)
+        private Node Insert(Node node, T value)
+        {
+            if(node == null)
+            {
+                return new Node(value);
+            }
+
+            int compare = node.Value.CompareTo(value);
+
+            if(compare > 0)
+            {
+                node.Left = this.Insert(node.Left, value);
+            }
+            else
+            {
+                node.Right = this.Insert(node.Right, value);
+            }
+            
+            return null;
+        }
+
+        public bool lins(T value)
         {
             throw new NotImplementedException();
         }
@@ -39,6 +96,20 @@ namespace BinarySearchTree
         public void EachInOrder(Action<T> action)
         {
             throw new NotImplementedException();
+        }
+
+
+        public class Node
+        {
+            public T Value { get; set; }
+
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+
+            public Node(T value)
+            {
+                this.Value = value;
+            }
         }
     }
 }
