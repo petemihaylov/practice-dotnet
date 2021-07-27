a30798ae483dff6f5120c61e927ad8d6ca2c4e0c
pepsm <peps.mihailov@gmail.com>
2021-07-27 22:30:22 +0200

    Add: Running migrations with EF - SQL light

--- PocketBook/Data/ApplicationDbContext.cs
+++ PocketBook/Data/ApplicationDbContext.cs
@@ -1,12 +1,18 @@
 using System;
 using Microsoft.EntityFrameworkCore;
+using PocketBook.Models;
 
 namespace PocketBook.Data {
     public class ApplicationDbContext : DbContext
     {
+        public virtual DbSet<User> Users {get; set;}
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
             
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
