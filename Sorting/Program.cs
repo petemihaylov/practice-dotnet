10030e27e560b81f614f058f15ae0c206a80f91c
pepsm <peps.mihailov@gmail.com>
2021-08-04 12:50:20 +0300

    Update: recurssion

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -5,10 +5,11 @@ namespace Sorting
 {
     class Program
     {
+
+
+
         static void Main(string[] args)
         {
-
-            // 1. Set up of the collection
 
             Console.Write("Enter the size of the collection : ");
             string input = null;
@@ -18,45 +19,99 @@ namespace Sorting
                 try
                 {
                     length = int.Parse(input);
+                    if (length < 0)
+                    {
+                        Console.Write("Enter the size of the collection : ");
+                        continue;
+                    }
                 }
                 catch
                 {
+                    Console.Write("Enter the size of the collection : ");
                     continue;
                 }
                 break;
             }
 
-            int[] intArray = new int[length];
-
-            for (int i = 0; i < intArray.Length; i++)
+            int[] arr;
+            if (ReadInput(out arr, length) == false)
             {
-
-                Console.Write($"Enter the n:{i+1} element :");
-                intArray[i] = int.Parse(Console.ReadLine());
+                return;
             }
 
-            for (int i = 0; i < intArray.Length; i++)
-            {
-                for (int j = 0; j < intArray.Length - i - 1; j++)
-                {
-                    Console.WriteLine($"{intArray[j]} > {intArray[j+1]}");
-                    if (intArray[j] > intArray[j + 1])
-                    {
-                        // swap
-                        int temp = intArray[j];
-                        intArray[j] = intArray[j + 1];
-                        intArray[j + 1] = temp;
-                    }
-                }
-            }
-
-            Array.ForEach(intArray, x => System.Console.Write($"{x} "));
 
 
+            BubbleSort(arr);
+            PrintResult(arr, "BubbleSort");
 
+
+            BubbleSortRecursive(arr, length);
+            PrintResult(arr, "BubbleSortRecursive");
 
         }
 
+        static void PrintResult(int[] arr, string algoName)
+        {
+            System.Console.WriteLine($"=> {algoName} <=");
+            Array.ForEach(arr, x => System.Console.Write($"{x} "));
+            System.Console.WriteLine("\n");
+        }
+
+        static bool ReadInput(out int[] arr, int length)
+        {
+            arr = new int[length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                Console.Write($"Enter element({i + 1}): ");
+                try
+                {
+                    arr[i] = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    System.Console.WriteLine("Invalid input, please try again!");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+
+        static void BubbleSort(int[] arr)
+        {
+            // Bubble sort
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        // swap
+                        swap(ref arr[j], ref arr[j + 1]);
+                    }
+                }
+            }
+        }
+        static void BubbleSortRecursive(int[] arr, int n)
+        {
+            // Base case
+            if (n == 1)
+                return;
+
+            for (int i = 0; i < n - 1; i++)
+                if (arr[i] < arr[i + 1])
+                    swap(ref arr[i], ref arr[i + 1]);
+
+            BubbleSortRecursive(arr, n - 1);
+        }
+        static void swap(ref int n1, ref int n2)
+        {
+            int temp = n1;
+            n1 = n2;
+            n2 = temp;
+        }
 
     }
 }
